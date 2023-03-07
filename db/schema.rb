7c2d843a597ee5dac04a74587b7365e732a48493
kenty51107 <iken51107@gmail.com>
2023-03-08 05:55:41 +0900

    [fix] データベースを作り直す

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_125748) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_03_07_192231) do
->>>>>>> feature/devise
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
